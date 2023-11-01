888344eaf3a0000f32d3c11f8d1f355d0c60c56a
mathewallen02 <mathewallenb@gmail.com>
2023-11-01 02:50:06 -0500

    Added Chat Gpt Support

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
 import os
+import openai
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
+load_dotenv()
 
-#declares what the bot is allowed to do
+openAiKey = os.getenv("OPENAIKEY")
+
+#Defines what the bot is allowed to do
+#Gives bot privileged abilites such as reading message content
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
 
-
-#confirms that bot is online
+#Prints a command to terminal that confirms that bot is online
 @client.event
 async def on_ready():
     print('Bot Online')
 
-#message handler
+#Function that gets a response from openAI based on a user query 
+def generate_response(query):
+    openai.api_key = openAiKey
+    response = openai.Completion.create(
+        model = "gpt-3.5-turbo", 
+        prompt = query
+    )
+    print(response)
+
+    return response.choices[0].text
+
+#Message handler
 @client.event
 async def on_message(message: str):
     #returns if message is posted by the bot itself
@@ -26,11 +41,10 @@ async def on_message(message: str):
     #prints message to console
     print(message.content)
 
-    #prints repsonse
-    response = 'hi'
+    #takes user message and gets a response from open ai
+    query = message.content
+    response = generate_response(query)
     await message.channel.send(response)
 
-
-load_dotenv()
-
+#runs bot 
 client.run(os.getenv("TOKEN"))
